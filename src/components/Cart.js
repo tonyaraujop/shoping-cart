d2477eafdefa387c490ae66cbe416f5f90c971e7
Antonio Paulino <tonnyarauujo@gmail.com>
2022-11-27 15:47:53 -0300

    Added cart component

---
+++ src/components/Cart.js
@@ -0,0 +1,11 @@
+const Cart = ({cart}) => {
+  return (
+    <>
+      <header><h1>Shopping Cart</h1></header>
+      <div>Your shopping cart have {cart.length} items added</div>
+    </>
+    
+  )
+}
+
+export default Cart
