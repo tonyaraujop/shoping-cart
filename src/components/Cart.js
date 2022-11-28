4b4fef7d2aa9818c55738c8a308e6a57d4fa3e72
Antonio Paulino <tonnyarauujo@gmail.com>
2022-11-28 11:03:47 -0300

    lint commit

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,10 +1,9 @@
-const Cart = ({cart}) => {
+const Cart = ({ cart }) => {
   return (
     <>
       <header><h1>Shopping Cart</h1></header>
       <div>Your shopping cart have {cart.length} items added</div>
     </>
-    
   )
 }
 
