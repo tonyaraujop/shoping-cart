0f6108e9ee462cd4be81ecc6e5fa23575d017fd8
Antonio Paulino <tonnyarauujo@gmail.com>
2022-11-28 11:06:55 -0300

    Added constants

---
+++ src/constants/constants.js
@@ -0,0 +1,16 @@
+const PRODUCTS = [
+  {
+    name: 'Feudum',
+    description: 'Game designed by Mark Swanson'
+  },
+  {
+    name: 'Mage Knight',
+    description: 'Game designed by Vlaada Chvátil'
+  },
+  {
+    name: 'Bonfire',
+    description: 'Game designed by Stefan Feld'
+  }
+]
+
+export default PRODUCTS
