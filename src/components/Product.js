11982194f6674d85a3d533816d8a7ace63e70add
Antonio Paulino <tonnyarauujo@gmail.com>
2022-12-03 12:54:33 -0300

    add removeFromCart function

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,20 +1,15 @@
-<<<<<<< HEAD
-export const Product = ({ product, addToCart = () => {}, hideAddToCartButton }) => {
-=======
 export const Product = ({ product, addToCart, removeFromCart}) => {
->>>>>>> 595920d (add removeFromCart function)
   const { name, description } = product
 
   return (
     <>
       <div>{name}: {description}</div>
-<<<<<<< HEAD
-      {!hideAddToCartButton && <button onClick={() => addToCart(product)}>Add to Cart</button>}
-=======
       {removeFromCart ?
         <button onClick={() => removeFromCart(product)}>Remove item from Cart</button> :
         <button onClick={() => addToCart(product)}>Add to Cart</button>}
->>>>>>> 595920d (add removeFromCart function)
+      {removeFromCart ?
+        <button onClick={() => removeFromCart(product)}>Remove item from Cart</button> :
+        <button onClick={() => addToCart(product)}>Add to Cart</button>}
     </>
   )
 }
